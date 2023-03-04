e74e3e63955be7306d5dcec5209270105643dcdd
mac <m1901321@edu.misis.ru>
2023-03-04 15:50:10 +0300

    v1

--- src/main/java/com/example/springsecurity/config/ConfigSpring.java
+++ src/main/java/com/example/springsecurity/config/ConfigSpring.java
@@ -1,8 +1,10 @@
 package com.example.springsecurity.config;
 
 
+import com.example.springsecurity.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -21,21 +22,32 @@ public class ConfigSpring {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .requestMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
+        UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
+                .roles(Role.ADMIN.name())
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles(Role.USER.name())
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
     @Bean
